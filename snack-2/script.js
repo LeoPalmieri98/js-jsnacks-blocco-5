00f50dbf41352dfc45702ae052e79c1036f341fb
leopii98 <rippergames98@gmail.com>
2025-11-21 18:12:53 +0100

    first commit

---
+++ snack-2/script.js
@@ -0,0 +1,13 @@
+const people = [
+  { name: 'Paolo', age: 35 },
+  { name: 'Giulia', age: 24 },
+  { name: 'Marco', age: 67 }
+];
+
+for (let i = 0; i < people.length; i++) {
+const element = people[i].name; 
+    console.log(element);
+}
+
+// Stampa in console tutti i nomi
+// Risultato: 'Paolo', 'Giulia', 'Marco'
