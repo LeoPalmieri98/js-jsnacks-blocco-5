2be6bbe8374af08cb3b92904d24fb81b3c3a93a8
leopii98 <rippergames98@gmail.com>
2025-11-21 18:44:22 +0100

    add-snack-4

--- snack-4/script.js
+++ snack-4/script.js
@@ -3,7 +3,12 @@ const posts = [
   { author: 'Luca', date: '30/6/2021', content: 'lorem ipsum...' },
   { author: 'Fabrizio', date: '12/9/2020', content: 'lorem ipsum...' },
   { author: 'Enrico', date: '09/12/2021', content: 'lorem ipsum...' },
-]
+];
+
+const authors = posts.map((post) => {
+  return post.author;
+});
+console.log(authors);
 
 // Crea un nuovo array coi nomi di tutti gli autori
 // Risultato: ['Marco', 'Luca', 'Fabrizio', 'Enrico']
