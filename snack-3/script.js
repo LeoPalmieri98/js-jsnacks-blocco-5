a200fa7597b9ec9402901f2c7a3ba4a314112f30
leopii98 <rippergames98@gmail.com>
2025-11-21 18:18:18 +0100

    add-snack3

--- snack-3/script.js
+++ snack-3/script.js
@@ -1,4 +1,10 @@
 const numbers = [2, 8, 4, 7, 2, 87];
+console.log(numbers);
+
+const incremento1 = numbers.map ((numbers) => {
+    return numbers + 1;
+});
+console.log(incremento1);
 
 // Crea un nuovo array in cui tutti i numeri siano incrementati di 1
 // Risultato: [3, 9, 5, 8, 3, 88]
